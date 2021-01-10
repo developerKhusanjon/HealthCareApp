1e5001976cc2c8612f53cfcaa86a5d4aa8299680
Khusanjon Tuychiboyev <developerkhusanjon@gmail.com>
2021-01-10 15:32:15 +0500

    initial_commit

---
+++ src/test/java/io/developerkhusanjon/HealthCareApp/HealthCareAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.developerkhusanjon.HealthCareApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
